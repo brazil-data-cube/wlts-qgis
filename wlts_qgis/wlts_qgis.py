b7227eed0dbecb53249d076a1a9f4e0b684587ac
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2020-12-04 14:56:44 -0300

    revised imports close #31

--- wlts_qgis/wlts_qgis.py
+++ wlts_qgis/wlts_qgis.py
@@ -21,9 +21,7 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
-from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+
 from qgis.core import QgsProject
 from qgis.gui import QgsMapToolEmitPoint
 
