d379c97b7e5c4c44d125c7e71ff055adad816f01
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2021-02-11 12:07:21 -0300

    [#49] Fixing manifest error and add isort check

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,4 @@
+# pydocstyle wlts_plugin setup.py && \
 isort wlts_plugin setup.py --check-only --diff && \
 check-manifest --ignore ".drone.yml,.readthedocs.yml" && \
 sphinx-build -qnW --color -b doctest wlts_plugin/help/source wlts_plugin/help/_build && \
--- wlts_plugin/test/test_02_wlts_qgis_dialog.py
+++ wlts_plugin/test/test_02_wlts_qgis_dialog.py
@@ -18,10 +18,6 @@ from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox
 
 from wlts_plugin.wlts_qgis_dialog import WltsQgisDialog
 
-from .utilities import get_qgis_app
-
-QGIS_APP = get_qgis_app()
-
 
 class WltsQgisDialogTest(unittest.TestCase):
     """Test dialog works."""
